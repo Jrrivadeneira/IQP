3ba0b3d1021393f2e07ffc4b03bf47003c8b5805
jack <jrrivadeneira@wpi.edu>
2015-11-03 13:52:41 -0500

    Documented EMT class.

--- src/model/EMT.java
+++ src/model/EMT.java
@@ -5,6 +5,10 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class EMT {
+	/**
+	 * EMT class used to store data about the users. 
+	 * Written by Jack Rivadeneira.
+	 */
 	private int id;
 	private int EMSUnitID;
 	private String name;
@@ -13,10 +17,26 @@ public class EMT {
 	private String email;
 	private final String validationString = "I have a dream that one day every valley shall be exalted, and every hill and mountain shall be made low, the rough places will be made plain, and the crooked places will be made straight; and the glory of the Lord shall be revealed and all flesh shall see it together.";
 
+	/**
+	 * Generates a totally empty EMT
+	 */
 	private EMT() {
 
 	}
 
+	/**
+	 * Constructor for the EMT. Used to create an EMT that will be saved to the
+	 * database with the given parameters. The password itself is not stored in
+	 * the database. Instead, the password is used to encrypt a validation
+	 * String that must be decrypted to sign in.
+	 * 
+	 * @param ID
+	 * @param EMSUNITID
+	 * @param Name
+	 * @param Username
+	 * @param Password
+	 * @param Email
+	 */
 	public EMT(int ID, int EMSUNITID, String Name, String Username,
 			String Password, String Email) {
 		this.id = ID;
@@ -27,7 +47,24 @@ public class EMT {
 		this.email = Email;
 	}
 
-	public static EMT generateEMT(int ID, int EMSUNITID, String Name,
+	/**
+	 * Creates an EMT as it would be loaded from a database.
+	 * 
+	 * @param ID
+	 *            - sets the ID of the emt
+	 * @param EMSUNITID
+	 *            - sets the EMSUnitID of the emt
+	 * @param Name
+	 *            - sets the name of the EMT
+	 * @param Username
+	 *            - sets the username of the EMT
+	 * @param Password
+	 *            - sets the byte array of the emt
+	 * @param Email
+	 *            - sets the Email of the EMT
+	 * @returns an EMT with the given data.
+	 */
+	protected static EMT generateEMT(int ID, int EMSUNITID, String Name,
 			String Username, byte[] Password, String Email) {
 		EMT e = new EMT();
 		e.id = ID;
@@ -36,10 +73,16 @@ public class EMT {
 		e.username = Username;
 		e.password = Password;
 		e.email = Email;
-
 		return e;
 	}
 
+	/**
+	 * Does not actually set the password! it takes a password p and encrypts
+	 * the validation string for login.
+	 * 
+	 * @param p
+	 *            password to be used to encrypt
+	 */
 	private void setPassword(String p) {
 		char pa = 'A';
 		while (p.length() != 16) {
@@ -55,10 +98,17 @@ public class EMT {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// System.out.println(this.password.length);
 	}
 
-	public boolean validate(String password) {
+	/**
+	 * attempts to decrypt the validation string using the given password in
+	 * order to validate a login.
+	 * 
+	 * @param password
+	 *            - the password used to decrypt
+	 * @returns true if the validation succeeded.
+	 */
+	protected boolean validate(String password) {
 		char c = 'A';
 		while (password.length() < 16) {
 			password += c;
@@ -81,13 +131,11 @@ public class EMT {
 		return false;
 	}
 
-	public static void main(String[] args) {
-		// System.out.println(randomString());
-		// EMT e = new EMT(1, 1, "Jack", "psychomadeye", "Databean",
-		// "me@example.com");
-		// System.out.println(new String(e.password));
-	}
-
+	/**
+	 * generates a random string to add security to the hash
+	 * 
+	 * @returns a random string.
+	 */
 	private static String randomString() {
 		char c[] = new char[64];
 		int i = 0;
@@ -99,26 +147,56 @@ public class EMT {
 		return new String(c);
 	}
 
+	/**
+	 * getter for ID
+	 * 
+	 * @returns the ID of this EMT
+	 */
 	public int getID() {
 		return this.id;
 	}
 
+	/**
+	 * getter for EMSUnitID
+	 * 
+	 * @returns the EMSUnitID of this EMT
+	 */
 	public int getEMSUnitID() {
 		return this.EMSUnitID;
 	}
 
+	/**
+	 * getter for Name
+	 * 
+	 * @returns the name of this EMT
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * getter for username
+	 * 
+	 * @returns the ID username this EMT
+	 */
 	public String getUsername() {
 		return this.username;
 	}
 
+	/**
+	 * getter for password
+	 * 
+	 * @returns the encrypted validation string of this EMT
+	 */
 	public byte[] getPassword() {
 		return this.password;
 	}
 
+	/**
+	 * getter for Email
+	 * 
+	 * @returns the Email of this EMT
+	 */
 	public String getEmail() {
 		return this.email;
 	}
