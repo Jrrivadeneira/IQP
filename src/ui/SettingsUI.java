01086c35265b73f79f7e3a0836d5c974ffe0b893
jack <jrrivadeneira@wpi.edu>
2015-11-05 14:48:48 -0500

    Created the UI-settings class

---
+++ src/ui/SettingsUI.java
@@ -0,0 +1,20 @@
+package ui;
+
+import control.Controller;
+
+public class SettingsUI extends FrameUI{
+
+	/**
+	 * Written by Jack Rivadeneira
+	 */
+	private static final long serialVersionUID = -6221117603324451662L;
+
+	public SettingsUI(Controller c) {
+		super(c);
+	}
+
+	public void buildUI() {
+		
+	}
+
+}
