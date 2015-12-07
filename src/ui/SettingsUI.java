4f833a5767c3d33580449f1dfe8552008824e171
jack <jrrivadeneira@wpi.edu>
2015-12-07 16:20:50 -0500

    Now tells user when the login fails!

--- src/ui/SettingsUI.java
+++ src/ui/SettingsUI.java
@@ -13,7 +13,6 @@ public class SettingsUI extends FrameUI {
 	 */
 	private static final long serialVersionUID = -6221117603324451662L;
 	JButton changePassword, uiTweaks, addHospital, back;
-	
 
 	public SettingsUI(Controller c) {
 		super(c);
@@ -26,31 +25,35 @@ public class SettingsUI extends FrameUI {
 
 	public void buildUI() {
 		this.setTitle("SettingsUI");
-		GridLayout layout = new GridLayout(2,4);
-		
+		GridLayout layout = new GridLayout(2, 4);
+
 		layout.setHgap(core.Configuration.uiSpacing);
 		layout.setVgap(core.Configuration.uiSpacing);
-		
+
 		this.setLayout(layout);
-		
+
 		this.changePassword = new JButton("Change Password");
 		this.changePassword.setName("change password");
 		this.changePassword.addActionListener(this.ctrl);
+		this.changePassword.setFont(core.Configuration.buttonFont);
 		this.add(this.changePassword);
-		
+
 		this.uiTweaks = new JButton("UI Tweaks");
 		this.uiTweaks.setName("ui tweaks");
 		this.uiTweaks.addActionListener(this.ctrl);
+		this.uiTweaks.setFont(core.Configuration.buttonFont);
 		this.add(this.uiTweaks);
-		
+
 		this.addHospital = new JButton("Add Hospital");
 		this.addHospital.setName("add hospital");
 		this.addHospital.addActionListener(this.ctrl);
+		this.addHospital.setFont(core.Configuration.buttonFont);
 		this.add(this.addHospital);
-		
+
 		this.back = new JButton("Back");
 		this.back.setName("back");
 		this.back.addActionListener(this.ctrl);
+		this.back.setFont(core.Configuration.buttonFont);
 		this.add(back);
 	}
 
