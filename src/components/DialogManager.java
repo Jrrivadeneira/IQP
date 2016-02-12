c331547ab58556f7ed91cf8068ce3a9c7516857d
jack <jrrivadeneira@wpi.edu>
2016-02-12 10:31:34 -0500

    Most of the program has been wiped away: package model - deleted. package control - deleted. package ui - deleted. package core - deleted. package components - 2 remaining. Months of work gone in a single commit. God help me.

--- src/components/DialogManager.java
+++
@@ -1,7 +0,0 @@
-package components;
-
-public final class DialogManager {
-	public static void showPinDialog(){
-		new PinDialog();
-	}
-}
--- src/model/Model.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Model {
-	public void say(String s);
-	
-}
